e3c5538de5e48c0651ac0640162e56cc7b9bcc1d
s20171103186 <s20171103186@admindeiMac-34.local>
2018-10-19 20:10:21 +0800

    超简单计算器
    编写一个计算器

--- calculate0/ViewController.swift
+++ calculate0/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func addtion(_ sender: UIButton) {
+        var x = 0
+        x = Int(number1.text!)!
+        var y = 0
+        y = Int(number2.text!)!
+        var z: Int = 0
+        z = x + y
+        number3.text = "\(z)"
+    }
+    @IBAction func reduce(_ sender: UIButton) {
+        var x = 0
+        x = Int(number1.text!)!
+        var y = 0
+        y = Int(number2.text!)!
+        var z: Int = 0
+        z = x - y
+        number3.text = "\(z)"
+    }
+    @IBAction func multiply(_ sender: UIButton) {
+        var x = 0
+        x = Int(number1.text!)!
+        var y = 0
+        y = Int(number2.text!)!
+        var z: Int = 0
+        z = x * y
+        number3.text = "\(z)"
+    }
+    @IBAction func division(_ sender: UIButton) {
+        var x = 0
+        x = Int(number1.text!)!
+        var y = 0
+        y = Int(number2.text!)!
+        var z: Int = 0
+        z = x / y
+        number3.text = "\(z)"
+    }
+    @IBOutlet weak var number1: UITextField!
+    @IBOutlet weak var number2: UITextField!
+    @IBOutlet weak var number3: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        number1.text = "0"
+        number3.text = "0"
+        number2.text = "0"
     }
 
 
